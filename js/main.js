ec6220dffe4ed6e71c76b8dfa6db1528fee63290
RAM YADAV <moniyadav2823@gmail.com>
2021-04-30 17:27:09 +0530

    loader added and enter button edited

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 function callDictionaryApi() {
+    document.getElementById('loader').style.display = "block"
+    document.getElementById('request-button').disabled = true
     var input = document.getElementById('inputedWord').value
     axios.post('https://shabdo-ki-dictionary.herokuapp.com/createDictionaryPage', {'word': input})
     .then(function (response) {
         console.log('received----',response);
         if (response.data.data[0].definition) {
-            return document.getElementById('detail').innerHTML = response.data.data[0].definition
+            document.getElementById('detail').innerHTML = response.data.data[0].definition
+            document.getElementById('loader').style.display = "none"
+            return document.getElementById('request-button').disabled = false
+
         }
         document.getElementById('detail').innerHTML = response.data.data[0].meaning
+        document.getElementById('loader').style.display = "none"
+        document.getElementById('request-button').disabled = false
     })
     .catch(function (error) {
         // handle error
